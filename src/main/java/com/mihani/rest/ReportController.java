c93c06cc37be6ec68c4fc8212353597b025a01e1
moha-kun <naitmoussamohamed@gmail.com>
2023-03-26 16:41:09 +0000

    fix some related typo to Bricoleur exception 2

--- src/main/java/com/mihani/rest/ReportController.java
+++ src/main/java/com/mihani/rest/ReportController.java
@@ -1,21 +1,12 @@
 package com.mihani.rest;
 
-import com.mihani.Exceptions.BricoleurAlreadyExistsException;
-import com.mihani.Exceptions.BricoleurNotFoundException;
-import com.mihani.dtos.BricoleurProfileDto;
-import com.mihani.entities.Bricoleur;
 import com.mihani.entities.Report;
 import com.mihani.dtos.ReportModel;
-import com.mihani.services.BricoleurServiceImpl;
 import com.mihani.services.ReportService;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReportController {
